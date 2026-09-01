611026098baf15f9b6a17b4646958dfc6c7bb9ee
SakaKrib <sakahaoke@gmail.com>
2026-09-01 23:01:58 +0300

    Configure Channels WebSocket application

--- backend/config/asgi.py
+++ backend/config/asgi.py
@@ -1,5 +1,20 @@
 import os
-from django.core.asgi import get_asgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'config.settings')
-application = get_asgi_application()
+
+from django.core.asgi import get_asgi_application
+from channels.auth import AuthMiddlewareStack
+from channels.routing import ProtocolTypeRouter, URLRouter
+
+from apps.core.routing import websocket_urlpatterns
+from apps.core.ws_auth import CookieJWTWebSocketMiddleware
+
+
+django_application = get_asgi_application()
+
+application = ProtocolTypeRouter({
+    'http': django_application,
+    'websocket': CookieJWTWebSocketMiddleware(
+        AuthMiddlewareStack(URLRouter(websocket_urlpatterns))
+    ),
+})
