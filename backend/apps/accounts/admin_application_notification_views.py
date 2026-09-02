117e0d4511e43eee60a2cddc81ed9c550b46a4a8
SakaKrib <sakahaoke@gmail.com>
2026-09-02 15:57:06 +0300

    Remove unused Profile import from notification endpoint

--- backend/apps/accounts/admin_application_notification_views.py
+++ backend/apps/accounts/admin_application_notification_views.py
@@ -4,7 +4,6 @@ from rest_framework.response import Response
 from rest_framework.views import APIView
 
 from apps.accounts.authorization import is_admin
-from apps.accounts.models import Profile
 from apps.core.email_services import queue_email
 
 
