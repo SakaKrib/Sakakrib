411ea7494b9b1e31559f999634d24f7465442ef2
SakaKrib <sakahaoke@gmail.com>
2026-09-02 22:40:22 +0300

    Route invoice payment WebSocket

--- backend/apps/core/routing.py
+++ backend/apps/core/routing.py
@@ -1,7 +1,9 @@
 from django.urls import re_path
 
 from .chat_consumers import MovingChatConsumer
+from .payment_consumers import PaymentStatusConsumer
 
 websocket_urlpatterns = [
     re_path(r"^ws/chat/(?P<conversation_id>[0-9a-fA-F-]+__[0-9a-fA-F-]+?)/$", MovingChatConsumer.as_asgi()),
+    re_path(r"^ws/payments/(?P<invoice_id>[0-9a-fA-F-]+)/$", PaymentStatusConsumer.as_asgi()),
 ]
