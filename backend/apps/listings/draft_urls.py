bf2296c9fc67ce9d8bed4e9f30f73d2a333014d7
SakaKrib <sakahaoke@gmail.com>
2026-09-02 18:21:42 +0300

    refactor(listings): replace draft table with listing-owned drafts

--- backend/apps/listings/draft_urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-
-from .draft_views import ListingDraftDetailView, ListingDraftListView
-
-urlpatterns = [
-    path('', ListingDraftListView.as_view(), name='listing-draft-list-create'),
-    path('<uuid:draft_id>/', ListingDraftDetailView.as_view(), name='listing-draft-detail'),
-]
